cddded34b1c2bac48bc04107555fd44f92a63811
o_omki <omkarmurty7@gmail.com>
2023-05-20 20:44:29 +0530

    Added appointments_collection

--- utils/database.py
+++ utils/database.py
@@ -8,6 +8,8 @@ MONGODO_URI = os.getenv('MONGO_URI')
 DATABASE_NAME = os.getenv('DATABASE_NAME')
 USER_COLLECTION_NAME = os.getenv('USERS_COLLECTION')
 JOURNAL_COLLECTION_NAME = os.getenv('JOURNAL_COLLECTION')
+COUNSELLOR_COLLECTION_NAME = os.getenv('COUNSELLOR_COLLECTION')
+APPOINTMENT_COLLECTION_NAME = os.getenv("APPOINTMENT_COLLECTION")
 
 client = MongoClient(MONGODO_URI)
 
@@ -18,3 +20,9 @@ def get_user_collection():
 
 def get_journal_collection():
     return db[JOURNAL_COLLECTION_NAME]
+
+def get_counsellor_collection():
+    return db[COUNSELLOR_COLLECTION_NAME]
+
+def get_appointments_collection():
+    return db[APPOINTMENT_COLLECTION_NAME]
